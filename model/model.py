bd9d9da3d9b4f8739c8028f3cf3505ce4262cc64
Lauren Choi <laurenchoi@gmail.com>
2022-04-25 20:01:18 -0400

    add method sigs to model

--- model/model.py
+++ model/model.py
@@ -1,5 +1,27 @@
-class RestorationModel():
+import tensorflow as tf
+import numpy as np
+from tensorflow.keras import Model
+
+class RestorationModel(tf.keras.Model):
     def __init__(self):
-        self.optimizer = 0 # add optimizer
+        super(Model, self).__init__()
     
-    
+    def call(self):
+        pass
+
+    def loss(self):
+        pass
+
+def train(model, train_inputs, train_labels):
+    pass
+
+def test(model, test_inputs, test_labels):
+    pass
+
+def main():
+    model = RestorationModel()
+    train(model, None, None)
+    test(model, None, None)
+
+if __name__ == '__main__':
+    main()
