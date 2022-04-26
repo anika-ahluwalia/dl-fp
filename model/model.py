79709a7d1fa6fed7745306ecc64b461dcbb0fe10
Lauren Choi <laurenchoi@gmail.com>
2022-04-25 20:05:12 -0400

    change class sig

--- model/model.py
+++ model/model.py
@@ -4,24 +4,17 @@ from tensorflow.keras import Model
 
 class RestorationModel(tf.keras.Model):
     def __init__(self):
-        super(Model, self).__init__()
+        super(RestorationModel, self).__init__()
     
+    @tf.function
     def call(self):
         pass
 
     def loss(self):
         pass
 
-def train(model, train_inputs, train_labels):
-    pass
-
-def test(model, test_inputs, test_labels):
-    pass
-
 def main():
     model = RestorationModel()
-    train(model, None, None)
-    test(model, None, None)
 
 if __name__ == '__main__':
     main()
