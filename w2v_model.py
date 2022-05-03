2cf8658958c5deb785b8f8c44625fd2c220047e6
naomipark3 <naomi_park@brown.edu>
2022-05-03 11:25:51 -0400

    word2vec attempt

--- w2v_model.py
+++ w2v_model.py
@@ -3,19 +3,25 @@ import numpy as np
 from tensorflow.keras import Model
 
 class Word2VecModel(tf.keras.Model):
-    def __init__(self):
+    def __init__(self, vocab_size, embedding_size):
         super(Word2VecModel, self).__init__()
 
         self.batch_size = 120
 
         self.learning_rate = 0.01
         self.optimizer = tf.keras.optimizers.Adam(learning_rate=self.learning_rate)
+        
+        self.embedding = tf.keras.layers.Embedding(vocab_size)
+        self.mlp = tf.keras.layers.Dense(vocab_size) #multi-layered perceptron!
     
     @tf.function
     # inputs is a list of size batch size that has a lists of all of the words in the reviews
     def call(self, inputs):
         # TODO: Naomi and Galen fill in Word2Vec
         print("Word2Vec")
+        embedding = self.embedding(inputs)
+        logits = self.mlp(embedding)
+        return tf.nn.softmax(logits)
 
     # hw 3 
     def loss(self, logits, labels):
