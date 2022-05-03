792919c547d33c0fd72bb9a988cf195606f16057
anika <anika.ahluwalia@gmail.com>
2022-05-02 20:46:45 -0400

    added accuracy and loss

--- model.py
+++ model.py
@@ -10,5 +10,12 @@ class AnalysisModel(tf.keras.Model):
     def call(self):
         pass
 
-    def loss(self):
-        pass
+    # hw 3 
+    def loss(self, logits, labels):
+        prob = tf.keras.losses.sparse_categorical_crossentropy(labels, logits)
+        loss = tf.reduce_mean(tf.cast(prob, tf.float32))
+        return loss
+
+    def accuracy(self, predictions, labels):
+        correct_predictions = tf.equal(tf.argmax(predictions, 1), tf.argmax(labels, 1))
+        return tf.reduce_mean(tf.cast(correct_predictions, tf.float32))
